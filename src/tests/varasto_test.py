5071816cebb7901c9029af9b0693de7bec7771e1
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-11-02 21:16:05 +0200

    testit 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,45 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_lisataan_negatiivinen_luku(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+    
+    def test_otetaan_negatiivinen_luku(self):
+        self.varasto.lisaa_varastoon(10)
+
+        self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_lisataan_liikaa(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_otetaan_liikaa(self):
+        self.varasto.ota_varastosta(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    
+    def test_tilavuus_negatiivinen(self):
+        self.varasto = Varasto(-1)
+
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+    
+    def test_alku_saldo_negatiivinen(self):
+        Varasto(10, -2)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_str(self):
+        self.varasto.lisaa_varastoon(4)
+
+        self.varasto.ota_varastosta(1)
+        self.assertEqual(str(self.varasto), "saldo = 3, vielä tilaa 7")
+
