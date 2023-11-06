a2134f383b940e30d03d0930ef46b6bb8b5ae9d0
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-11-06 21:26:19 +0200

    ei näy lint kohtaa git hub actioineissa niin yritän uudelleen

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää
+    # ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
